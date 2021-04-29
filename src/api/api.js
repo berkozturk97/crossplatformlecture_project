09fef4593f57b63ad23be0e5d6b730f56559e713
seckinbrke <seckin_brke@hotmail.com>
2021-04-30 01:07:31 +0300

    Adding APIs

---
+++ src/api/api.js
@@ -0,0 +1,93 @@
+import axios from 'axios';
+import Global from '../constants/Global';
+
+
+export const getCategories = () => {
+  return new Promise((resolve, reject) => {
+    let REQUEST_URL = Global.BASE_URL + 'categories';
+    axios
+      .get(REQUEST_URL)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(null);
+        //console.warn(err);
+      });
+  });
+};
+
+export const getProducts = () => {
+  return new Promise((resolve, reject) => {
+    let REQUEST_URL = Global.BASE_URL + 'products';
+    axios
+      .get(REQUEST_URL)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(null);
+        //console.warn(err);
+      });
+  });
+};
+
+export const getProductById = ({ id = null }) => {
+  return new Promise((resolve, reject) => {
+    let REQUEST_URL = Global.BASE_URL + 'products/' + id;
+    axios
+      .get(REQUEST_URL)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(null);
+        //console.warn(err);
+      });
+  });
+};
+
+export const addCategories = ({ body = null }) => {
+  return new Promise((resolve, reject) => {
+    let REQUEST_URL = Global.BASE_URL + 'categories';
+    axios
+      .post(REQUEST_URL, body)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(null);
+        //console.warn(err);
+      });
+  });
+};
+
+export const updateCategories = ({ body = null }) => {
+  return new Promise((resolve, reject) => {
+    let REQUEST_URL = Global.BASE_URL + 'categories';
+    axios
+      .put(REQUEST_URL, body)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(null);
+        //console.warn(err);
+      });
+  });
+};
+
+export const getOrders = () => {
+  return new Promise((resolve, reject) => {
+    let REQUEST_URL = Global.BASE_URL + 'orders';
+    axios
+      .get(REQUEST_URL)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(null);
+        //console.warn(err);
+      });
+  });
+};
